8362c2c2ba5f847c47ee53806f9467ef9c6fe1c0
uusa35 <uusa35@gmail.com>
2025-06-24 17:20:39 +0300

    updates

--- app/Http/Controllers/Api/ChildController.php
+++ app/Http/Controllers/Api/ChildController.php
@@ -69,7 +69,7 @@ class ChildController extends Controller
      */
     public function show(Child $child)
     {
-        if ($child->user_id === request()->user()->id) {
+        if ($child->user_id == request()->user()->id) {
             return response()->json($child->load('stage'), 200);
         }
         return response()->json(['message' => 'Unauthorized'], 403);
